8d602fea94fd620be746b2b74089548f85e078a6
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-09-21 10:55:21 -0700

    add header which includes main logo and functional hamburger icon (open and close)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './scss/input.scss';
+import $ from "jquery";
+
+$(document).ready(function() {
+  $("#hamburger-icon").click(function() {
+    $(this).hide();
+    $("#x-icon").show();
+  });
+  $("#x-icon").click(function() {
+    $(this).hide();
+    $("#hamburger-icon").show();
+  });
+
+});
