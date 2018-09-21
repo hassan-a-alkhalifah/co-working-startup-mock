1980d03d41e1e2aabe51476b93eb7fd94ee0c339
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-09-21 15:19:21 -0700

    add all content for landing page content

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ $(document).ready(function() {
   $("#hamburger-icon").click(function() {
     $(this).hide();
     $("#x-icon").show();
+    $("#dropdown-menu-container").show();
   });
   $("#x-icon").click(function() {
     $(this).hide();
     $("#hamburger-icon").show();
+    $("#dropdown-menu-container").hide();
   });
 
 });
